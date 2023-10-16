203e16cd2b9d3c15d15ed7c89bb10270a903dce6
Muhammad Yasir <yasirabid96@gmail.com>
2023-10-16 14:25:26 +1000

    Flask Project updated

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('home.html')
+
+
+    
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
+    
+    
