1bbe6e4890aa7a836fe3434ef74fbd0ce8a13f6c
Muhammad Yasir <yasirabid96@gmail.com>
2023-10-20 12:29:58 +1000

    Flask Project updated

--- app.py
+++ app.py
@@ -3,21 +3,52 @@ from flask import Flask,render_template,jsonify
 
 app = Flask(__name__)
 
+JOBS=[
+    {
+        'id':1,
+        'title':'Python Developer',
+        'location':'Nairobi',
+        'salary':50000,
+    },
+    {
+        'id':2,
+        'title':'Data Systems Analyst',
+        'location':'Nairobi',
+        'salary':'RS 50000'
+    }
+    ,
+    {
+        'id':3,
+        'title':'Data Scientist',
+        'location':'Nairobi',
+        'salary':'RS 50000'
+    },
+        {
+             
+        'id':4,
+        'title':'Data Engineer',
+        'location':'Nairobi',
+        'salary':'RS 50000' 
+        }
+    
+]
 @app.route('/')
 def index():
     """
     This function renders the home page of the web application.
     """
-    return render_template('home.html')
+    return render_template('home.html',jobs=JOBS,company='Yasir')
 
-def create():
-    """
-    This function creates a new item in the web application.
-    """
-    pass
+@app.route('/api/jobs')
+def get_jobs():
+    return jsonify(JOBS)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=5002)
+    
+    
+    
+    
     
     
     
