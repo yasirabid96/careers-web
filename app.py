649ec2b5710c000b8ed77228e7b139b11c7f703f
Muhammad Yasir <yasirabid96@gmail.com>
2023-10-22 00:16:10 +1000

    MYSQL CONNECTED ADDED DATA DYNAMIC ADDED SQLALC

--- app.py
+++ app.py
@@ -1,73 +1,44 @@
 from flask import Flask,render_template,jsonify
-from database import engine,text
+from database import engine,text,load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS=[
+# JOBS=[
     
     
-    {
-        'id':1,
-        'title':'Python Developer',
-        'location':'Nairobi',
-        'salary':50000,
-    },
-    {
-        'id':2,
-        'title':'Data Systems Analyst',
-        'location':'Nairobi',
-        'salary':'RS 50000'
-    }
-    ,
-    {
-        'id':3,
-        'title':'Data Scientist',
-        'location':'Nairobi',
-        'salary':'RS 50000'
-    },
-        {
+#     {
+#         'id':1,
+#         'title':'Python Developer',
+#         'location':'Nairobi',
+#         'salary':50000,
+#     },
+#     {
+#         'id':2,
+#         'title':'Data Systems Analyst',
+#         'location':'Nairobi',
+#         'salary':'RS 50000'
+#     }
+#     ,
+#     {
+#         'id':3,
+#         'title':'Data Scientist',
+#         'location':'Nairobi',
+#         'salary':'RS 50000'
+#     },
+#         {
              
-        'id':4,
-        'title':'Data Engineer',
-        'location':'Nairobi',
-        'salary':'RS 50000' 
-        }
+#         'id':4,
+#         'title':'Data Engineer',
+#         'location':'Nairobi',
+#         'salary':'RS 50000' 
+#         }
     
-]
+# ]
 
 
 
    
-def load_jobs_from_db():
-    with engine.connect() as conn:
-        rs = conn.execute(text("SELECT * from jobs"))
-        #print("Type Result", type(rs))
 
-        results = rs.all()
-        #print("Type Results All", type(results))
-
-        column_names = rs.keys()  # Get the column names
-        #print("Column Names:", column_names) 
-
-        final_results = [dict(zip(column_names, row)) for row in results] # Convert rows into dictionaries with column names as keys
-        print(final_results)
-        return final_results
-        # results_dict=[]
-        # for row in fina l_results:
-        #     results_dict.append(dict(row))
-    
-        # print("Final Results:", type(final_results))
-        # print("Final Results:", final_results)
-        # dict_result = dict(final_results[0])
-        # dict_result_type = type(dict_result)
-        # print("Dict Result Type:", dict_result_type)
-        # print("Dict Result:", dict_result)
-        
-        
-        #print("Results Dict:", type(results_dict))
-        #print("Results Dict:", results_dict)
-        
-    
 @app.route('/')
 def index():
     
--- database.py
+++ database.py
@@ -10,30 +10,35 @@ engine = create_engine(db_string, connect_args={
     }
 })
 
-with engine.connect() as conn:
-    rs = conn.execute(text("SELECT * from jobs"))
-    print("Type Result", type(rs))
 
-    results = rs.all()
-    print("Type Results All", type(results))
 
-    column_names = rs.keys()  # Get the column names
-    print("Column Names:", column_names)
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        rs = conn.execute(text("SELECT * from jobs"))
+        #print("Type Result", type(rs))
 
-    final_results = [dict(zip(column_names, row)) for row in results] # Convert rows into dictionaries with column names as keys
-   
-    results_dict=[]
-    for row in final_results:
-        results_dict.append(dict(row))
-   
-    # print("Final Results:", type(final_results))
-    # print("Final Results:", final_results)
-    # dict_result = dict(final_results[0])
-    # dict_result_type = type(dict_result)
-    # print("Dict Result Type:", dict_result_type)
-    # print("Dict Result:", dict_result)
+        results = rs.all()
+        #print("Type Results All", type(results))
+
+        column_names = rs.keys()  # Get the column names
+        #print("Column Names:", column_names) 
+
+        final_results = [dict(zip(column_names, row)) for row in results] # Convert rows into dictionaries with column names as keys
+        print(final_results)
+        return final_results
+        # results_dict=[]
+        # for row in fina l_results:
+        #     results_dict.append(dict(row))
     
+        # print("Final Results:", type(final_results))
+        # print("Final Results:", final_results)
+        # dict_result = dict(final_results[0])
+        # dict_result_type = type(dict_result)
+        # print("Dict Result Type:", dict_result_type)
+        # print("Dict Result:", dict_result)
+        
+        
+        #print("Results Dict:", type(results_dict))
+        #print("Results Dict:", results_dict)
+        
     
-    print("Results Dict:", type(results_dict))
-    print("Results Dict:", results_dict)
-
